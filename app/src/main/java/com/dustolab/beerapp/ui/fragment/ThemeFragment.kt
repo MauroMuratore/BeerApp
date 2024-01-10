44e777fa82536849588025c941cf0eb3ccd14014
Murotek <m.muratore001@studenti.unibs.it>
2024-01-10 18:25:07 +0100

    add button back al theme

--- app/src/main/java/com/dustolab/beerapp/ui/fragment/ThemeFragment.kt
+++ app/src/main/java/com/dustolab/beerapp/ui/fragment/ThemeFragment.kt
@@ -2,16 +2,23 @@ package com.dustolab.beerapp.ui.fragment
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.findFragment
+import androidx.navigation.fragment.findNavController
 import com.dustolab.beerapp.R
 
 class ThemeFragment: Fragment(R.layout.fragment_theme){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val btnBack = requireView().findViewById<Button>(R.id.btn_back)
+        btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
 
         val dayTheme = requireView().findViewById<LinearLayout>(R.id.day_theme)
         val nightTheme = requireView().findViewById<LinearLayout>(R.id.night_theme)

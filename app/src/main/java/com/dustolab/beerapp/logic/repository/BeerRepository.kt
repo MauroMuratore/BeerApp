5599091c99c7d5e9be4a89ceb5c30e49551d3e17
nikbet98 <38vadbBS98>
2024-01-10 16:03:04 +0100

    bottone per vedere i bar con la birra aggiunto

--- app/src/main/java/com/dustolab/beerapp/logic/repository/BeerRepository.kt
+++ app/src/main/java/com/dustolab/beerapp/logic/repository/BeerRepository.kt
@@ -13,7 +13,8 @@ import kotlinx.coroutines.Dispatchers
 
 class BeerRepository(
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.Default,
-    private val dbReference: CollectionReference = Firebase.firestore.collection(PATH)
+    private val dbReference: CollectionReference = Firebase.firestore.collection(PATH),
+    private val barDbReference: CollectionReference = Firebase.firestore.collection((BAR_PATH))
 ) {
 
 
@@ -26,6 +27,12 @@ class BeerRepository(
     fun getBeer(uid: String): Task<QuerySnapshot>{
         return dbReference
             .whereEqualTo(UID, uid)
+            .get()
+    }
+
+    fun getBeerBars(uid: String): Task<QuerySnapshot> {
+        return barDbReference
+            .whereEqualTo(BAR_BEERS,uid)
             .get()
     }
 
@@ -83,6 +90,8 @@ class BeerRepository(
     companion object{
         const val UID : String = "uid"
         const val PATH : String = "beers"
+        const val BAR_PATH: String = "bars"
+        const val BAR_BEERS: String = "beerList/uid"
         const val FAVORITE_BY: String = "favoriteBy"
         const val RATING: String = "rating"
         const val STYLE : String = "style"
